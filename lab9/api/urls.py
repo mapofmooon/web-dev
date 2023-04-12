9147411e03cc42ae8dc4536ebbf329c78a794a28
Nargiz <123298526+n-sain@users.noreply.github.com>
2023-04-12 21:17:01 +0600

    Add files via upload

---
+++ lab9/api/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('companies/', views.CompanyList.as_view()),
+    path('companies/<int:pk>/', views.CompanyDetail.as_view()),
+    path('companies/<int:pk>/vacancies/', views.VacancyList.as_view()),
+    path('vacancies/', views.VacancyList.as_view()),
+    path('vacancies/<int:pk>/', views.VacancyDetail.as_view()),
+    path('vacancies/top_ten/', views.TopTenVacancies.as_view()),
+    path('api/', include('api.urls')),
+]
